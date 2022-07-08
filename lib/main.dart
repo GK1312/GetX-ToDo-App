f41ff5646081b0c756d1c02703c8b1cfeb1e3c66
GK1312 <gauravsk1234@gmail.com>
2022-07-08 11:09:17 +0530

    folder structure, extension & color & iconData added, get materialApp and GetStorage created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:getx_to_do_app/screens/home/home_screen.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:getx_to_do_app/app/modules/home/view.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -9,12 +12,10 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'GetX ToDo App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
+      home: HomePage(),
     );
   }
 }
