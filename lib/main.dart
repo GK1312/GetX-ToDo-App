0ffa8698043001ff7e089fa933f1c9b5968ea141
GK1312 <gauravsk1234@gmail.com>
2022-07-08 15:52:21 +0530

    add task view, controller and icons added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:getx_to_do_app/app/data/services/storage/services.dart';
+import 'package:getx_to_do_app/app/modules/home/binding.dart';
 import 'package:getx_to_do_app/app/modules/home/view.dart';
 
 void main() async {
@@ -14,10 +16,12 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'GetX ToDo App',
-      home: HomePage(),
+      home: const HomePage(),
+      initialBinding: HomeBinding(),
+      builder: EasyLoading.init(),
     );
   }
 }
